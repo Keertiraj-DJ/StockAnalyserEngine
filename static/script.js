6deb8cfd1e29acf94e8e3ce95724e630cf4ec68c
Keertiraj DJ <keertirajdj94@gmail.com>
2024-07-08 11:05:07 +0530

    Updating base url for render deployement

--- static/script.js
+++ static/script.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const stockTableBody = document.querySelector('#stock-table tbody');
     const loaderItem = document.getElementById('loader');
 
-    const base_url = 'https://integral-christa-keesha-2df2a8e4.koyeb.app'
+    //const base_url = 'tender-nightingale-keesha-36143a60.koyeb.app'
+    const base_url = 'https://stockanalyserengine.onrender.com'
     //const base_url = 'http://127.0.0.1:5000'
 
     let watchlistStocks = [];
